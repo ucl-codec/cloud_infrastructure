4272c898a8a4da61fd2ff4a26592a93d8ed5831d
Tom Doel <tom.doel@codechoreography.co.uk>
2023-03-07 21:44:33 +0000

    Add setup stacks

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import os
 import aws_cdk as cdk
 from aws_fbm.fbm_network_stack import FbmNetworkStack
 from aws_fbm.fbm_node_stack import FbmNodeStack
+from aws_fbm.stacks.fbm_node_setup_stack import FbmNodeSetupStack
 
 
 def get_environment() -> cdk.Environment:
@@ -25,12 +26,17 @@ network = FbmNetworkStack(
     env=get_environment()
 )
 
+node_a_import_bucket_name = "clinical-node-a-import-bucket"
+node_a_setup = FbmNodeSetupStack(
+    scope=app, id="FbmNodeStackA", site_name="Clinical Node A",
+    bucket_name=node_a_import_bucket_name)
 node_a = FbmNodeStack(
     scope=app,
     construct_id="FbmNodeStackA",
+    site_name = "Clinical Node A",
     stack_name="FbmNodeStackA",
     dns_domain="passian.clinicala",
-    bucket_name="clinical-node-a",
+    bucket_name=node_a_import_bucket_name,
     description="PASSIAN Fed-BioMed stack for clinical node A",
     network_number=1,
     network_stack=network,
@@ -38,12 +44,17 @@ node_a = FbmNodeStack(
     env=get_environment()
 )
 
+node_b_import_bucket_name = "clinical-node-b-import-bucket"
+node_b_setup = FbmNodeSetupStack(
+    scope=app, id="FbmNodeStackB", site_name="Clinical Node B",
+    bucket_name=node_b_import_bucket_name)
 node_b = FbmNodeStack(
     scope=app,
     construct_id="FbmNodeStackB",
+    site_name = "Clinical Node B",
     stack_name="FbmNodeStackB",
     dns_domain="passian.clinicalb",
-    bucket_name="clinical-node-b",
+    bucket_name=node_b_import_bucket_name,
     description="PASSIAN Fed-BioMed stack for clinical node B",
     network_number=2,
     network_stack=network,

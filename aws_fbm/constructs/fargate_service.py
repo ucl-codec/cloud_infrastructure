2a2a25f5b61b940eef442f5fd2fb40c2c5d742d4
Tom Doel <tom.doel@codechoreography.co.uk>
2023-03-08 07:58:18 +0000

    Add volume mounting to the FargateService class so it can be used for the node service

--- aws_fbm/constructs/fargate_service.py
+++ aws_fbm/constructs/fargate_service.py
@@ -8,6 +8,8 @@ from aws_cdk import aws_ecr_assets as ecr_assets
 from aws_cdk import aws_ecs as ecs
 from aws_cdk import aws_ec2 as ec2
 
+from aws_fbm.fbm_file_system import FbmVolume
+
 
 class FargateService(Construct):
     """Create a Fargate service for running a Docker container"""
@@ -26,9 +28,11 @@ class FargateService(Construct):
         port: int,
         permitted_client_ip_range: str,
         entry_point: Optional[Sequence[str]] = None,
-        environment: Optional[Mapping[str, str]] = None
+        environment: Optional[Mapping[str, str]] = None,
+        volumes: Optional[Sequence[FbmVolume]] = None
     ):
         super().__init__(scope, id)
+        volumes = volumes or []
 
         self.service_def = FbmFargateServiceDef(
             scope=self,
@@ -40,6 +44,8 @@ class FargateService(Construct):
             memory_limit_mib=memory_limit_mib,
             ephemeral_storage_gib=ephemeral_storage_gib
         )
+        for volume in volumes:
+            self.service_def.add_volume(volume=volume)
         self.container = self.service_def.add_docker_container(
             docker_image=docker_image_asset,
             name=task_name,
@@ -49,8 +55,21 @@ class FargateService(Construct):
             environment=environment
         )
 
+        # Expose ports from the container
         self.container.add_port_mappings(
             ecs.PortMapping(container_port=port))
+
+        # Add mount points to the container
+        for volume in volumes:
+            self.container.add_mount_points(ecs.MountPoint(
+                source_volume=volume.volume_name,
+                container_path=volume.mount_dir,
+                read_only=False
+            ))
+
+        # Create the service
         self.service = self.service_def.create_service()
+
+        # Open the service to incoming connections
         self.service.connections.allow_from(
             ec2.Peer.ipv4(permitted_client_ip_range), ec2.Port.tcp(port))

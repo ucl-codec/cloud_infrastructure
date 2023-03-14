0ae2b8375f6c3a4f8ff7ac277661136209802f3c
Tom Doel <tom.doel@codechoreography.co.uk>
2023-03-14 10:17:11 +0000

    Comments and reformatting

--- aws_fbm/fbm_base_stack.py
+++ aws_fbm/fbm_base_stack.py
@@ -1,16 +1,17 @@
-from aws_cdk import Environment
-from aws_cdk import Stack
-
-from constructs import Construct
+from aws_cdk import Environment, Stack
 from aws_cdk import aws_ec2 as ec2
-import aws_cdk.aws_ssm as ssm
+from aws_cdk import aws_ssm as ssm
 from aws_cdk import aws_route53 as route53
+from constructs import Construct
 
 
 class FbmBaseStack(Stack):
+    """CDK stack base class defining common VPC, VPN and DNS configuration for
+    FBM network and node stacks"""
 
     def __init__(self, scope: Construct, construct_id: str,
                  stack_name: str,
+    def __init__(self, scope: Construct, id: str,
                  site_name: str,
                  dns_domain: str,
                  description: str,
@@ -24,6 +25,8 @@ class FbmBaseStack(Stack):
         self.site_name = site_name
         self.dns_domain = dns_domain
 
+        # Select IP ranges for VPC and VPN. These must be non-overlapping
+        # across the entire system, since the VPCs are peered
         self.cidr_range = f"10.{2*network_number}.0.0/16"
         self.vpn_cidr_range = f"10.{2*network_number + 1}.0.0/22"
         self.dns_ip = f"10.{2*network_number}.0.2"
@@ -31,6 +34,9 @@ class FbmBaseStack(Stack):
             self, "passian-fbm-vpn-server-cert-arn")
 
         self.add_vpc()
+
+        # Some resources must be created in a specific subnet, so we
+        # arbitrarily choose the first
         self.first_subnet_id = self.vpc.select_subnets(
             subnet_group_name="private").subnet_ids[0]
         self.add_vpn()
@@ -39,11 +45,11 @@ class FbmBaseStack(Stack):
     def add_vpc(self):
         """Create VPC"""
 
+        # S3 gateway is required to use the ECR because we are using
+        # a private subnet with no internet connectivity
         self.vpc = ec2.Vpc(
             self,
             "VPC",
-            # S3 gateway is required to use the ECR because we are using
-            # a private subnet with no internet connectivity
             gateway_endpoints={
                 "S3": ec2.GatewayVpcEndpointOptions(
                     service=ec2.GatewayVpcEndpointAwsService.S3
@@ -64,50 +70,39 @@ class FbmBaseStack(Stack):
         # These endpoints are required in order to use the ECS
         # because we are using a private subnet with no internet connectivity
         self.add_interface_endpoint(
-            vpc=self.vpc,
             name="AgentEndpoint",
             service=ec2.InterfaceVpcEndpointAwsService.ECS_AGENT)
         self.add_interface_endpoint(
-            vpc=self.vpc,
             name="TelemetryEndpoint",
             service=ec2.InterfaceVpcEndpointAwsService.ECS_TELEMETRY)
         self.add_interface_endpoint(
-            vpc=self.vpc,
             name="EcsEndpoint",
             service=ec2.InterfaceVpcEndpointAwsService.ECS)
 
         # These endpoints are required in order to use the ECR and logging
         # because we are using a private subnet with no internet connectivity
         self.add_interface_endpoint(
-            vpc=self.vpc,
             name="ECRDockerEndpoint",
             service=ec2.InterfaceVpcEndpointAwsService.ECR_DOCKER)
         self.add_interface_endpoint(
-            vpc=self.vpc,
             name="ECREndpoint",
             service=ec2.InterfaceVpcEndpointAwsService.ECR)
         self.add_interface_endpoint(
-            vpc=self.vpc,
             name="CloudWatchEndpoint",
             service=ec2.InterfaceVpcEndpointAwsService.CLOUDWATCH)
         self.add_interface_endpoint(
-            vpc=self.vpc,
             name="CloudWatchLogsEndpoint",
             service=ec2.InterfaceVpcEndpointAwsService.CLOUDWATCH_LOGS)
         self.add_interface_endpoint(
-            vpc=self.vpc,
             name="CloudWatchEventsEndpoint",
             service=ec2.InterfaceVpcEndpointAwsService.CLOUDWATCH_EVENTS)
         self.add_interface_endpoint(
-            vpc=self.vpc,
             name="SSMEndpoint",
             service=ec2.InterfaceVpcEndpointAwsService.SSM)
         self.add_interface_endpoint(
-            vpc=self.vpc,
             name="Ec2MessagesEndpoint",
             service=ec2.InterfaceVpcEndpointAwsService.EC2_MESSAGES)
         self.add_interface_endpoint(
-            vpc=self.vpc,
             name="SSMMessagesEndpoint",
             service=ec2.InterfaceVpcEndpointAwsService.SSM_MESSAGES)
 
@@ -123,22 +118,20 @@ class FbmBaseStack(Stack):
     def add_vpn(self):
         """Add VPN endpoint"""
 
+        # Note this will automatically creates authorization rule
         self.vpn_endpoint = self.vpc.add_client_vpn_endpoint(
             "VpnEndpoint",
             cidr=self.vpn_cidr_range,
             server_certificate_arn=self.vpn_cert_arn,
-            # ToDo: change this to an authorisation rule that restricts access
-            # to a subnet which just corresponds to the GUI/researcher
-            authorize_all_users_to_vpc_cidr=True,  # Automatically creates authorization rule
+            authorize_all_users_to_vpc_cidr=True,
             client_certificate_arn=self.vpn_cert_arn,
             self_service_portal=False,
             split_tunnel=True,
             dns_servers=[self.dns_ip],
             description=f"{self.stack_name} Client VPN Endpoint",
-            # vpc_subnets=""
         )
 
-    def add_interface_endpoint(self, vpc, name, service):
+    def add_interface_endpoint(self, name, service):
         endpoint = self.vpc.add_interface_endpoint(id=name, service=service)
         endpoint.connections.allow_from(ec2.Peer.ipv4(self.vpc.vpc_cidr_block),
                                         endpoint.connections.default_port)

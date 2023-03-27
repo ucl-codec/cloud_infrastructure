5979bc054e1ad93461a98c24ece218dd7aaae221
Tom Doel <tom.doel@codechoreography.co.uk>
2023-03-27 09:26:08 +0100

    Update comments in deploy scripts

--- scripts/deploy_dev.sh
+++ scripts/deploy_dev.sh
@@ -6,9 +6,5 @@ REPO_DIR=$(dirname $(dirname $(realpath ${0})))
 # Start from repo dir
 cd "${REPO_DIR}"
 
-# Update test stacks
+# Deploy system defined in config/dev.cfg
 cdk deploy --all --profile passian --context config=dev
-#cdk deploy TestNetworkStack TestNetworkServiceStack TestResearcherServiceStack TestNodeStackA TestNodeServiceStackA TestPeeringStack --profile passian --context config=dev
-
-# Full deployment
-#cdk deploy TestNetworkStack TestNodeASetup TestNodeStackA --profile passian
--- scripts/deploy_prod.sh
+++ scripts/deploy_prod.sh
@@ -6,5 +6,5 @@ REPO_DIR=$(dirname $(dirname $(realpath ${0})))
 # Start from repo dir
 cd "${REPO_DIR}"
 
-# Deploy main stacks
+# Deploy system defined in config/prod.cfg
 cdk deploy --all --profile passian --context config=prod
